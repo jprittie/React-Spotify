3357619149946118631e3ae4c6789124a8a938d1
jprittie <eyewallop@gmail.com>
2017-05-13 16:53:36 -0400

    Updates routing

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,20 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Home from './Home';
+import Results from './Results';
+import Single from './Single';
+
 
 class Main extends Component {
+
+
 	render () {
 		return (
 		  <main className="main-content clearfix">
 		    <Switch>
 		      <Route exact path='/' component={Home}/>
+          <Route path="/search/:searchTerm" component={Results} />
+          <Route path="/album/:albumId/:albumSlug" component={Single} />
 
 		    </Switch>
       </main>
