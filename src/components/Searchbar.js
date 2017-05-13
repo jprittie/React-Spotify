2366b3f5b9d0675b4c3613f6e23449afe0ee688d
jprittie <eyewallop@gmail.com>
2017-05-13 14:59:12 -0400

    Changes for to htmlFor

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,7 +5,7 @@ class Searchbar extends Component {
 	render() {
 		return (
       <form className="search-form">
-        <label className="is-hidden" for="search">Search for music...</label>
+        <label className="is-hidden" htmlFor="search">Search for music...</label>
         <input type="search" name="search" id="search" placeholder="Search for Music..."/>
 
         <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
