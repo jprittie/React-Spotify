aac72a0a2a4e343830a2874d2fc160b792dfe89f
jprittie <eyewallop@gmail.com>
2017-05-13 14:59:25 -0400

    Passes props to Header

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 	render () {
 		return (
 	  	<div>
-    		<Header />
+    		<Header siteName="AlbumSearch" />
     		<Main />
   		</div>
 		);
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,18 @@ import Searchbar from './Searchbar';
 
 
 class Header extends Component {
+
+	static propTypes = {
+		siteName: React.PropTypes.string.isRequired
+
+	}
+
+
 	render() {
 		return (
 		  <div className="main-header">
 		    <div className="inner">
-		      <h1 className="main-title">AlbumSearch</h1>
+		      <h1 className="main-title">{this.props.siteName}</h1>
 		      <Searchbar />
       	</div>
     	</div>
